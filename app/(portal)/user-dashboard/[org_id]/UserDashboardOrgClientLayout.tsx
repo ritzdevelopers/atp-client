935ddfa3e7c540882c21d4e39f79baa2ce409cda
itzz-aryan-121 <arhanrizvi9@gmail.com>
2026-05-26 15:47:37 +0530

    update

--- app/(portal)/user-dashboard/[org_id]/UserDashboardOrgClientLayout.tsx
+++ app/(portal)/user-dashboard/[org_id]/UserDashboardOrgClientLayout.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import DraggableClock from "@/components/portal-dashboard/user-layout/DraggableClock";
+// import DraggableClock from "@/components/portal-dashboard/user-layout/DraggableClock";
 import LeftSideBar from "@/components/portal-dashboard/user-layout/LeftSideBar";
 import RightMainSide from "@/components/portal-dashboard/user-layout/RightMainSide";
 
 function UserDashboardComponent({ children }: { children: React.ReactNode }) {
   return (
     <main className="relative min-h-screen w-full overflow-x-hidden bg-slate-50">
-      <DraggableClock />
+      {/* <DraggableClock /> */}
       <LeftSideBar />
       <div className="w-full pb-[calc(4.5rem+env(safe-area-inset-bottom))] pt-14 lg:pb-0 lg:pl-[250px] lg:pt-0">
         <RightMainSide>{children}</RightMainSide>
